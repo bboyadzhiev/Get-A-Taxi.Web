d52abe42a64746c82629e30dd33c3183c2f2762d
Borislav Boyadzhiev <bboyadzhiev@gmail.com>
2015-05-12 21:46:54 +0300

    Taxi stands web api update

--- Backend/Get_A_Taxi.Web/Controllers/WebAPI/TaxiStandsController.cs
+++ Backend/Get_A_Taxi.Web/Controllers/WebAPI/TaxiStandsController.cs
@@ -7,33 +7,95 @@ using System.Net.Http;
 using System.Web.Http;
 using AutoMapper.QueryableExtensions;
 using Get_A_Taxi.Web.Models;
+using Get_A_Taxi.Web.Infrastructure;
+using Get_A_Taxi.Models;
+using Get_A_Taxi.Web.Infrastructure.Services.Contracts;
 
 namespace Get_A_Taxi.Web.Controllers.WebAPI
 {
     [Authorize]
     [RoutePrefix("api/TaxiStands")]
-    public class TaxiStandsController : BaseApiController
+    public class TaxiStandsController : BaseApiController, IRESTController<TaxiStandDTO>
     {
+        private ITaxiStandService taxiStandService;
+        private const int TAXI_STANDS_RESULTS_DEFAULT_COUNT = 10;
 
-        public TaxiStandsController(IGetATaxiData data)
+        public TaxiStandsController(IGetATaxiData data, ITaxiStandService taxiStandService)
             : base(data)
         {
+            this.taxiStandService = taxiStandService;
+        }
+
+
+        public IHttpActionResult Get()
+        {
+            return BadRequest();
         }
 
         /// <summary>
-        /// Get all Taxi Stands for a district
+        /// Get taxi stand by id
         /// </summary>
-        /// <param name="districtId">The District's ID</param>
-        /// <returns>A list of taxi stands</returns>
+        /// <param name="id">The id of the taxi stand</param>
+        /// <returns>The taxi stand's data model</returns>
         [HttpGet]
-        public IHttpActionResult Get(int districtId)
+        public IHttpActionResult Get(int id)
         {
-            var taxiStands = this.Data.Stands.All()
-                .Where(s => s.District.DistrictId == districtId)
+            var taxiStand = this.taxiStandService.GetAll().Where(t => t.TaxiStandId == id).Project().To<TaxiStandDTO>();
+            return Ok(taxiStand);
+        }
+
+        /// <summary>
+        /// Get all Taxi Stands for the nearest district by given coordinates
+        /// </summary>
+        /// <param name="lat">The latitude</param>
+        /// <param name="lon">The longitude</param>
+        /// <returns>List of top 10 nearest locations</returns>
+        [HttpGet]
+        public IHttpActionResult Get(double lat, double lon)
+        {
+            var taxiStands = this.taxiStandService.GetAll();
+            var taxiStandsDTOList = this.taxiStandService.ByNearestLocation(taxiStands, lat, lon)
+                .Take(TAXI_STANDS_RESULTS_DEFAULT_COUNT)
                 .Project().To<TaxiStandDTO>()
                 .ToList();
 
-            return Ok(taxiStands);
+            return Ok(taxiStandsDTOList);
         }
+
+        /// <summary>
+        /// Used by taxi driver to list a page of all taxi stands in his district
+        /// </summary>
+        /// <param name="page">Page number</param>
+        /// <returns>List of taxi stands</returns>
+        [AuthorizeRoles(UserRole = UserRoles.Driver)]
+        [HttpGet]
+        public IHttpActionResult GetPaged(int page)
+        {
+            var driver = this.GetUser();
+            var taxiStands = this.taxiStandService.GetAll();
+            var taxiStandsDTOList = this.taxiStandService.ByDistrict(taxiStands, driver.District.DistrictId)
+                .Skip(page * TAXI_STANDS_RESULTS_DEFAULT_COUNT)
+                .Take(TAXI_STANDS_RESULTS_DEFAULT_COUNT)
+                .Project().To<TaxiStandDTO>()
+                .ToList();
+            return Ok(taxiStandsDTOList);
+        }
+
+
+        public IHttpActionResult Post(TaxiStandDTO model)
+        {
+            return BadRequest();
+        }
+
+        public IHttpActionResult Put(TaxiStandDTO model)
+        {
+            return BadRequest();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            return BadRequest();
+        }
+
     }
 }
