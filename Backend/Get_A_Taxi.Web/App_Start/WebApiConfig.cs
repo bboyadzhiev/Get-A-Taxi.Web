c2432bbca23827b017c97db46245ccc67e19aaad
Borislav Boyadzhiev <bboyadzhiev@gmail.com>
2015-05-20 00:37:21 +0300

    test

--- Backend/Get_A_Taxi.Web/App_Start/WebApiConfig.cs
+++ Backend/Get_A_Taxi.Web/App_Start/WebApiConfig.cs
@@ -17,7 +17,7 @@ namespace Get_A_Taxi.Web
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-
+            config.Filters.Add(new AuthorizeWebApiRolesAttribute());
             // Remove the XML formatter
             //config.Formatters.Remove(config.Formatters.XmlFormatter);
 
