46f2492dcd31cb0265f8587e0b1fb9b11f215425
Borislav Boyadzhiev <bboyadzhiev@gmail.com>
2015-09-13 18:17:11 +0300

    updates

--- Backend/Get_A_Taxi.Web/WebAPI/BaseApiController.cs
+++ Backend/Get_A_Taxi.Web/WebAPI/BaseApiController.cs
@@ -47,62 +47,6 @@
             var user = this.Data.Users.All().FirstOrDefault(u => u.Id == userId);
             return user;
         }
-
-        //private ApplicationUser _member;
-
-        
-        ////try  n
-        //private ApplicationUserManager _userManager;
-
-        //public ApplicationUserManager UserManager1
-        //{
-        //    get
-        //    {
-        //        return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
-        //    }
-        //    private set
-        //    {
-        //        _userManager = value;
-        //    }
-        //}
-
-        //// try 2
-        //public ApplicationUserManager UserManager2
-        //{
-        //    get { return HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>(); }
-        //}
-        //public string UserIdentityId
-        //{
-        //    get
-        //    {
-        //        //var user = UserManager.FindByName(User.Identity.Name);
-        //        //return user.Id;
-        //        var username = RequestContext.Principal.Identity.GetUserName();
-                
-        //       // var userobject = UserManager.FindByNameAsync(uident.Name);
-        //     //   ClaimsIdentity identity = (ClaimsIdentity)User.Identity;
-        //     //   string username = identity.Claims.First().Value;
-        //        var userobject = UserManager2.FindByNameAsync(username);
-        //        var userid = userobject.Result.Id;
-        //        return userid;
-        //    }
-        //}
-
-        //public ApplicationUser UserRecord
-        //{
-        //    get
-        //    {
-        //        if (_member != null)
-        //        {
-        //            return _member;
-        //        }
-        //        _member = UserManager2.FindByEmail(Thread.CurrentPrincipal.Identity.Name);
-        //        return _member;
-        //    }
-        //    set { _member = value; }
-        //}
-
-
         #endregion
     }
 }
